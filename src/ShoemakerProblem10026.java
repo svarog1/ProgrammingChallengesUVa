54792026b868b8118892758acc58ff722c9cb7f0
Santino De-Sassi <S92@pipe.ch>
2018-12-30 12:51:02 +0100

    Shoemaker version speed 0.090 nicer code splitted input and output.

--- src/ShoemakerProblem10026.java
+++ src/ShoemakerProblem10026.java
@@ -1,8 +1,8 @@
 import java.io.*;
-import  java.util.*;
+import java.util.*;
 
 class Main {
-    public static void main (String args[])  // entry point from OS
+    public static void main(String args[])  // entry point from OS
     {
         ShoemakerProblem10026 myWork = new ShoemakerProblem10026();  // create a dinamic instance
         myWork.Begin();            // the true entry point
@@ -10,135 +10,121 @@ class Main {
 }
 
 class ShoemakerProblem10026 {
-    static String ReadLn (int maxLg)  // utility function to read from stdin
+    static String ReadLn(int maxLg)  // utility function to read from stdin
     {
-        byte lin[] = new byte [maxLg];
+        byte lin[] = new byte[maxLg];
         int lg = 0, car = -1;
         String line = "";
 
-        try
-        {
-            while (lg < maxLg)
-            {
+        try {
+            while (lg < maxLg) {
                 car = System.in.read();
                 if ((car < 0) || (car == '\n')) break;
-                lin [lg++] += car;
+                lin[lg++] += car;
             }
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             return (null);
         }
 
         if ((car < 0) && (lg == 0)) return (null);  // eof
-        return (new String (lin, 0, lg));
+        return (new String(lin, 0, lg));
     }
 
-    public static void main (String args[])  // entry point from OS
+    public static void main(String args[])  // entry point from OS for testing
     {
         ShoemakerProblem10026 myWork = new ShoemakerProblem10026();  // create a dinamic instance
         myWork.Begin();            // the true entry point
     }
 
-    void Begin()
-    {
+    void Begin() {
         String input;
-        boolean isReadNumberOfAssignements=false;
-        boolean isReadNumberOfShomakers =false;
-        boolean skip=false;
+        boolean isReadNumberOfAssignments = false;
+        boolean isReadNumberOfShoemakers = false;
+        boolean skip = false;
 
-        Shoemaker[] shoemakers=null;
-        int shomakerToAdd=0;
+        Shoemaker[] shoemakers = null;
+        int shoemakerToAdd = 0;
 
-        while ((input = ShoemakerProblem10026.ReadLn (255)) != null)
-        {
-            if(skip)
-            {
-                skip=false;
-            }
-            else if (!isReadNumberOfShomakers)
-            {
-                shoemakers=new Shoemaker[Integer.parseInt(input)];
-                isReadNumberOfShomakers = true;
-                skip=true;
-            }
-            else if (!isReadNumberOfAssignements){
-                shoemakers[shomakerToAdd]=new Shoemaker(Integer.parseInt(input));
-                isReadNumberOfAssignements=true;
-            }
-            else{
-                String[] timeFine =input.split(" ");
-                Assignement assignement =new Assignement(Integer.parseInt(timeFine[0]),Integer.parseInt(timeFine[1]));
-                if (shoemakers[shomakerToAdd].addNext(assignement)){
-                    System.out.println(shoemakers[shomakerToAdd].getWorkOrder());
-                    shomakerToAdd++;
-                    skip=true;
-                    isReadNumberOfAssignements=false;
-                    if (shomakerToAdd==shoemakers.length)
-                    {
-                        return;
-
-                    }
-                    else {
-                        System.out.println("");
-                    }
+        //Read the inputs and generate classes.
+        while ((input = ShoemakerProblem10026.ReadLn(255)) != null) {
+            if (skip) {//skip empty column
+                skip = false;
+            } else if (!isReadNumberOfShoemakers) {//Read the number of shoemakers
+                shoemakers = new Shoemaker[Integer.parseInt(input)];
+                isReadNumberOfShoemakers = true;
+                skip = true;
+            } else if (!isReadNumberOfAssignments) { //read number of assignments for the active shoemaker
+                shoemakers[shoemakerToAdd] = new Shoemaker(Integer.parseInt(input));
+                isReadNumberOfAssignments = true;
+            } else { //Generate the Shoemakers and the Assignments
+                String[] timeFine = input.split(" ");
+                Assignment assignment = new Assignment(Integer.parseInt(timeFine[0]), Integer.parseInt(timeFine[1]));
+                if (shoemakers[shoemakerToAdd].addNext(assignment)) {// When the last Assignment go in the if
+                    shoemakerToAdd++;
+                    skip = true;
+                    isReadNumberOfAssignments = false;
+                    if (shoemakerToAdd==shoemakers.length){break;}
                 }
+            }
+        }
+
+        //Creat the correct output.
+        int counter=0;
+        while (true){
+            shoemakers[counter].printWorkOrder();
+            counter++;
+            if (counter<shoemakers.length) {
+                System.out.println("");
+            }
+            else {
+                break;
             }
         }
     }
 
-    class Shoemaker
-    {
-        private int toAdd=0;
-        public Assignement[] assignements;
-        public Shoemaker(int numberOfAssignements){
-            assignements=new Assignement[numberOfAssignements];
+    class Shoemaker {
+        int toAdd = 0;
+        Assignment[] assignments;
+
+        Shoemaker(int numberOfAssignements) {
+            assignments = new Assignment[numberOfAssignements];
         }
 
         //return was last entry ==true
-        public boolean addNext(Assignement assignement)
-        {
-            assignement.position=toAdd+1;
-            assignements[toAdd]=assignement;
+        boolean addNext(Assignment assignment) {
+            assignment.position = toAdd + 1;
+            assignments[toAdd] = assignment;
             toAdd++;
-            return  (toAdd==assignements.length ? true : false);
+            return (toAdd == assignments.length);
         }
 
-        public String getWorkOrder(){
-            String workOrder="";
-            Arrays.sort(assignements);
-            for (int i=0; i<assignements.length;i++)
-            {
-                if (i==assignements.length-1)
-                {
-                    workOrder+= assignements[i].position;
-                }
-                else {
-                    workOrder+= assignements[i].position +" ";
+        void printWorkOrder() {
+            StringBuilder sb = new StringBuilder();
+            Arrays.sort(assignments);
+            for (int i = 0; i < assignments.length; i++) {
+                if (i == assignments.length - 1) {
+                    sb.append(assignments[i].position);
+                } else {
+                    sb.append(assignments[i].position).append(" ");
                 }
 
             }
-            return workOrder;
+            System.out.println( sb.toString());
         }
 
     }
 
-    class Assignement implements Comparable<Assignement>{
-        final public int time;
-        final public int fine;
-        final public  double priority;
-        public int position;
-        public Assignement(int time, int fine)
-        {
-            this.time=time;
-            this.fine=fine;
-            priority=(double)fine/(double)time;
+    class Assignment implements Comparable<Assignment> {
+        final double priority;
+        int position;
+
+        private Assignment(int time, int fine) {
+            priority = (double) fine / (double) time;
         }
 
         @Override
-        public int compareTo(Assignement a)
-        {
-            return Double.compare(a.priority,this.priority);
+        public int compareTo(Assignment a) {
+            return Double.compare(a.priority, this.priority);
         }
 
     }
