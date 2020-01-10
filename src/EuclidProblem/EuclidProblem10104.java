3e50cdbd72247f4442ae4319300fd4c118ff379c
Santino De-Sassi <Santino.DeSassi@students.fhnw.ch>
2020-01-10 11:12:05 +0100

    Tug of ware 10032 first version

--- src/EuclidProblem/EuclidProblem10104.java
+++ src/EuclidProblem/EuclidProblem10104.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
 @SuppressWarnings("WrongPackageStatement")
 class Main {
     public static void main(String args[])  // entry point from OS
@@ -10,11 +11,18 @@ class Main {
         euclidProblem10104.start();
     }
 }
-
+*/
+/**
+ * Behandelt das Euclid Problem (10104) von UVa.
+ * Das Problem wurde mittels des Erweiterten euklidischen Algorithmus gelöst.
+ * https://de.wikipedia.org/wiki/Erweiterter_euklidischer_Algorithmus
+ * @author Santino De-Sassi (Username: Svarog1)
+ * @date 2020.01.01
+ */
 class GGTEntry {
     public long mult;
 
-    public GGTEntry(long a, long b, long mult, long rest) {
+    public GGTEntry( long mult) {
 
         this.mult = mult;
     }
@@ -50,42 +58,31 @@ class EuclidProblem10104 {
             }
             String[] numbers = input.split(" ");
             calc(Long.parseLong(numbers[0]), Long.parseLong(numbers[1]));
-
         }
-
-        //System.out.println("");
-
     }
 
+    //does the actual calculation
     public void calc(long a, long b) {
-        calcGGT(a, b);
-    }
 
-    public  void printLn(long x, long y, long d) {
-        System.out.println(x + " " + y + " " + d);
-    }
-
-    public  void calcGGT(long a, long b) {
-        ArrayList<GGTEntry> l = new ArrayList<>();
-        GGTEntry entry;
-        boolean isSwitched=false;
+        //special cases
         if (a == 0) {
             printLn(0, 1, b);
             return;
-        } else if (b == 0) {
+        } else if (b == 0) { //prevent divided by 0
             printLn(1, 0, a);
             return;
         } else if (a == b) {
             printLn(0, 1, a);
             return;
         }
-       if (a<b){
 
-       }
-       long ggt=0;
-       long tempa;
+        long ggt=0;
+        long tempa;
+        Long entry;
+        ArrayList<Long> l = new ArrayList<>();
+        //calc GGT
         while (true) {
-            entry = new GGTEntry(a, b, a / b, a % b);
+            entry = a / b;
             tempa=a;
             a = b;
 
@@ -102,19 +99,22 @@ class EuclidProblem10104 {
             return;
         }
 
-
+        //Calc X,Y
         long multX = 1;
-        long multY = -l.get(l.size() - 1).mult;
-
+        long multY = -l.get(l.size() - 1);
         long tempMultmx = 0;
-
         for (int i = l.size() - 2; i >= 0; i--) {
             tempMultmx = multY;
-            multY = multX + (multY * (-l.get(i).mult));
+            multY = multX + (multY * (-l.get(i)));
             multX = tempMultmx;
         }
         printLn(multX, multY, ggt);
     }
+
+    public  void printLn(long x, long y, long d) {
+        System.out.println(x + " " + y + " " + d);
+    }
+
 }
 
 
